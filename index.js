4837294636e9625becfb36c225adb31610be4537
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-05-02 22:34:28 -0700

    OOP weather

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-// ------------------------------------------------------
-// Developer Code
-
-// Get Element refererences
-const tempEl = document.getElementById('temp')
-const descEl = document.getElementById('desc')
-const formEl = document.getElementById('form')
-const zipInput = document.getElementById('zip')
-
-// Define event listeners
-formEl.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const zip = zipInput.value
-  getWeather(zip)
-})
-
-// Functions 
-function getWeather(apikey, zip) {
-  const apiKey = apiKey
-  const units = 'imperial'
-  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-  fetch(path)
-    .then(res => res.json())
-    .then(json => {
-      tempEl.innerHTML = json.main.temp
-      descEl.innerHTML = json.weather[0].description
-    })
-    .catch(err => console.log(err.message))
+class OpenWeather {
+  constructor(apikey, units='imperial') {
+    this.apikey = apikey
+    this.units = units
+  }
+  async getWeather(zip) {
+    const { apikey, units } = this
+    const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`
+  
+    // fetch data 
+    const response = await fetch(path)
+    // when that resolves we call reponse.json to stream json data
+    const json = await response.json() 
+    // return the data
+    return json
+  }
 }
-
-
-
-module.exports = getWeather
-      
