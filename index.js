edc7b66907e7645d470311fe74ed0f156de36b04
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-05-14 14:17:47 -0700

    update

--- index.js
+++ index.js
@@ -3,9 +3,46 @@ class OpenWeather {
     this.apikey = apikey
     this.units = units
   }
+
   async getWeather(zip) {
     const { apikey, units } = this
     const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apikey}&units=${units}`
+  
+    // fetch data 
+    const response = await fetch(path)
+    // when that resolves we call reponse.json to stream json data
+    const json = await response.json() 
+    // return the data
+    return json
+  }
+
+  async getWeatherByCity(city) {
+    const { apikey, units } = this
+    const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${units}`
+  
+    // fetch data 
+    const response = await fetch(path)
+    // when that resolves we call reponse.json to stream json data
+    const json = await response.json() 
+    // return the data
+    return json
+  }
+
+  async getWeatherByCord(lat, lon) {
+    const { apikey, units } = this
+    const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=${units}`
+  
+    // fetch data 
+    const response = await fetch(path)
+    // when that resolves we call reponse.json to stream json data
+    const json = await response.json() 
+    // return the data
+    return json
+  }
+
+  async getWeatherById(id) {
+    const { apikey, units } = this
+    const path = `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=${apikey}&units=${units}`
   
     // fetch data 
     const response = await fetch(path)
